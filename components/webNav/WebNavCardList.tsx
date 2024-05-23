44b0d3c8ae19aa063411c7c7a056fae7d3730d93
wangmeng <wangmeng@i72.com>
2024-05-23 17:32:40 +0800

    feat: update

--- components/webNav/WebNavCardList.tsx
+++ components/webNav/WebNavCardList.tsx
@@ -1,8 +1,8 @@
-import { WebNavigationListRow } from '@/lib/data';
+import { NavItemType } from '@/lib/data';
 
 import WebNavCard from './WebNavCard';
 
-export default function WebNavCardList({ dataList }: { dataList: WebNavigationListRow[] }) {
+export default function WebNavCardList({ dataList }: { dataList: NavItemType[] }) {
   return (
     <div className='grid grid-cols-2 gap-3 lg:grid-cols-5 lg:gap-4'>
       {dataList.map((item) => (
